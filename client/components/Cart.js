64870d7b697376fceec332dd8378030944a9a9b6
BHARADWAJ-MADDURI <bmadduri19@gmail.com>
2022-05-02 14:32:04 -0500

    checkout button modified for Guest

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { updateOrder, updateLineitem, guestCheckout } from "../store";
 import CartItem from "./CartItem";
+import { Link } from "react-router-dom";
 
 export class Cart extends Component {
   constructor() {
@@ -9,8 +10,14 @@ export class Cart extends Component {
   }
 
   render() {
-    const { cart, cartItems, updateOrder, updateLineitem, guestCheckout } =
-      this.props;
+    const {
+      auth,
+      cart,
+      cartItems,
+      updateOrder,
+      updateLineitem,
+      guestCheckout,
+    } = this.props;
 
     const Checkout = () => {
       if (cart.id) {
@@ -22,7 +29,7 @@ export class Cart extends Component {
         guestCheckout(cartItems);
       }
     };
-
+    console.log("auth", auth);
     if (!cartItems?.length) return <h1>Nothing in Cart</h1>;
 
     return (
@@ -32,7 +39,13 @@ export class Cart extends Component {
             return <CartItem cartItem={cartItem} key={cartItem.instrumentId} />;
           })}
         </ul>
-        <button onClick={Checkout}> Checkout </button>
+        {auth.id ? <button onClick={Checkout}> Checkout</button> : null}
+        {!auth.id ? (
+          <>
+            <button onClick={Checkout}> Checkout As Guest </button>{" "}
+            <Link to="/signup">Sign Up</Link>{" "}
+          </>
+        ) : null}
       </div>
     );
   }
@@ -46,12 +59,14 @@ const mapState = ({ orders, lineitems, auth }) => {
     );
     const cartItems = lineitems.filter((item) => item.orderId === cart?.id);
     return {
+      auth,
       cart,
       cartItems,
     };
   } else {
     const cart = JSON.parse(window.localStorage.getItem("cart"));
     return {
+      auth,
       cart,
       cartItems: lineitems,
     };
