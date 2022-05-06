9f8002aa434cf134e880321bd9beb70ced102ca0
Robert Albenda <ralbenda@gmail.com>
2022-05-06 13:14:17 -0400

    added select bar

--- client/components/Instruments.js
+++ client/components/Instruments.js
@@ -5,6 +5,14 @@ import { Link } from "react-router-dom";
 export const Instruments = ({ instruments, brands, categories }) => {
   return (
     <div>
+      <label for="sort">Sort by:</label>
+      <select name ='Sort'>
+       <option value="Ascending by Name">Ascending by Name</option>
+       <option value="Descending by Name">Descending by Name</option>
+       <option value="Ascending by Price">Ascending by Price</option>
+       <option value="Descending by Price">Descending by Price</option>
+      </select>
+  
       <ul>
         {instruments.map((instrument) => {
           const brand = brands.find((brand) => brand.id === instrument.brandId);
@@ -12,8 +20,8 @@ export const Instruments = ({ instruments, brands, categories }) => {
             categories.find(
               (category) => category.id === instrument.categoryId
             ) || {};
-          return (
-            <div key={instrument.id}>
+          return ( 
+          <div key={instrument.id}>
               <Link to="/brands/">{brand.name}</Link>-
               <Link to={`/instruments/${instrument.id}`}>
                 {instrument.name}
