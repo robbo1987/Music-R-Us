8ed38448e93e62a697349d1f9e695a0be7de24d4
BHARADWAJ-MADDURI <bmadduri19@gmail.com>
2022-05-09 11:42:44 -0500

    images resized

--- client/components/Categories.js
+++ client/components/Categories.js
@@ -11,8 +11,6 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
-import { maxHeight } from "@mui/system"
-
 
 export const Categories = ({ categories }) => {
   return (
